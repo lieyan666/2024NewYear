a30eced2afac9b07537f9610f795dab2228ef54a
lieyan666 <2102177341@qq.com>
2023-12-31 22:26:51 +0800

    Add Random Name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: Lieyan
  * @Date: 2023-12-31 20:40:02
  * @LastEditors: Lieyan
- * @LastEditTime: 2023-12-31 21:29:46
+ * @LastEditTime: 2023-12-31 22:14:36
  * @FilePath: /2024NewYear/app.js
  * @Description:
  * @Contact: QQ: 2102177341  Website: lieyan.space  Github: @lieyan666
@@ -15,7 +15,8 @@ const app = express();
 
 const port = 52024;
 
-var cnt = 0;
+var cntS = 0;
+var cntN = 0;
 
 // 从文件中读取句子
 async function readSentencesFromFile() {
@@ -30,8 +31,20 @@ async function readSentencesFromFile() {
     return [];
   }
 }
+async function readNamesFromFile() {
+    try {
+      const data = await fs.readFile("names.txt", "utf-8");
+      return data
+        .split("\n")
+        .map((line) => line.trim())
+        .filter((line) => line !== "");
+    } catch (error) {
+      console.error("Error reading names from file:", error.message);
+      return [];
+    }
+  }
 
-// 获取随机句子的函数
+// 获取随机句子
 async function getRandomSentence() {
   const sentences = await readSentencesFromFile();
   if (sentences.length === 0) {
@@ -40,6 +53,15 @@ async function getRandomSentence() {
   const randomIndex = Math.floor(Math.random() * sentences.length);
   return sentences[randomIndex];
 }
+// 获取随机名字
+async function getRandomName() {
+    const names = await readNamesFromFile();
+    if (names.length === 0) {
+      return "No name available.";
+    }
+    const randomIndex = Math.floor(Math.random() * names.length);
+    return names[randomIndex];
+  }
 
 function getTime() {
   // 获取当前时间
@@ -65,9 +87,15 @@ function getTime() {
 app.get("/api/random-sentence", async (req, res) => {
   const randomSentence = await getRandomSentence();
   let time = getTime();
-  console.log(`{${++cnt}} [${time}] : ${randomSentence}`);
+  console.log(`{${++cntS}} [${time}] : ${randomSentence}`);
   res.json({ sentence: randomSentence });
 });
+app.get("/api/random-name", async (req, res) => {
+    const randomName = await getRandomName();
+    let time = getTime();
+    console.log(`{${++cntN}} [${time}] : ${randomName}`);
+    res.json({ name: randomName });
+  });
 
 app.get("/web", (req, res) => {
   const filePath = path.join(__dirname, "web.html");
